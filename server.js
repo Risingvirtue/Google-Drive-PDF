0a1f7767f1f422c02e00c8df1e553e80b3ab4357
Your Name <you@example.com>
2018-10-29 14:50:55 -0700

    private

--- server.js
+++ server.js
@@ -18,11 +18,10 @@ app.get('/files', function(req,res){
 		//couldn't send \n
 		var private_key = req.headers.private_key;
 		
-		private_key = private_key.split('?').join('\n');
+		//private_key = private_key.split('\n').join('');
 		
 		var access = {client_email: client_email, private_key: private_key};
-		res.send(access);
-		return;
+		
 		var auth = getAuthorize(access);
 		
 		post = res;
